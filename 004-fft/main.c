5bb5233514327c6a44e369cd9d5cd035cd8a9964
Raspberry Pi <raspberrypisig@gmail.com>
2021-02-12 10:49:38 +1100

    poormans added to fft example

--- 004-fft/main.c
+++ 004-fft/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char) { 
+  reset_usb_boot(0, 0); 
+} // go to flash mode
+
 // Sample from the ADC continuously at a particular sample rate
 // and then compute an FFT over the data
 //
@@ -40,7 +44,7 @@ void sample(uint8_t *capture_buf);
 
 int main() {
   stdio_init_all();
-  sleep_ms(10000);
+  sleep_ms(1000);
   tud_cdc_set_wanted_char('\0');
 
   uint8_t cap_buf[NSAMP];
