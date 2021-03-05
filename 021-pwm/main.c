3432ae3dbe0bfa4ad0ec5366a40fbce02839150d
raspberrypisig <raspberrypisig@gmail.com>
2021-03-05 16:02:27 +1100

    Update main.c

--- 021-pwm/main.c
+++ 021-pwm/main.c
@@ -1,20 +1,28 @@
 
 #include "main.h"
 
+typedef struct {
+  float divider;
+  uint16_t wrap;
+} PWMSettings;
+
 #define SQUAREWAVE_PIN 20 // slice 2, channel A
 
 void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char) { 
   reset_usb_boot(0, 0); 
 } // go to flash mode
 
-void calculatePrescalerAndWrap(float freq) {
-  float divider  = 1.f;
+PWMSettings calculatePrescalerAndWrap(float freq) {
+  float divider  = -1.f;
   uint16_t wrap = 0;
+  PWMSettings settings;
    
   if (freq > 180000) {
     divider = 2.f*(48000000/freq);
     wrap = 2;
-    return;
+    settings.divider = divider;
+    settings.wrap = wrap;
+    return settings;
   }
   
   else if ((freq < 180000) && (freq > 800)) {
@@ -32,54 +40,15 @@ void calculatePrescalerAndWrap(float freq) {
   } else if (freq > 3) {
     divider = 255.f;
  else {
+    settings.divider = -1.f;
     return;
   }
     
- wrap = (48000000/divider) / freq; 
+ wrap = (48000000/divider) / freq;
+ settings.divider = divider;
+ settings.wrap = wrap;
+ return settings;
   
-  // Set up the flexible divider/compare
-  /*
-  uint16_t divider  = 1;
-  uint16_t compare = 0;
-  tc_clock_prescaler prescaler = TC_CLOCK_PRESCALER_DIV1;
-
-  if ((freq < 24000000) && (freq > 800)) {
-    divider = 1;
-    prescaler = TC_CLOCK_PRESCALER_DIV1;
-    compare = 48000000/freq;
-  } else if (freq > 400) {
-    divider = 2;
-    prescaler = TC_CLOCK_PRESCALER_DIV2;
-    compare = (48000000/2)/freq;
-  } else if (freq > 200) {
-    divider = 4;
-    prescaler = TC_CLOCK_PRESCALER_DIV4;
-    compare = (48000000/4)/freq;
-  } else if (freq > 100) {
-    divider = 8;
-    prescaler = TC_CLOCK_PRESCALER_DIV8;
-    compare = (48000000/8)/freq;
-  } else if (freq > 50) {
-    divider = 16;
-    prescaler = TC_CLOCK_PRESCALER_DIV16;
-    compare = (48000000/16)/freq;
-  } else if (freq > 12) {
-    divider = 64;
-    prescaler = TC_CLOCK_PRESCALER_DIV64;
-    compare = (48000000/64)/freq;
-  } else if (freq > 3) {
-    divider = 256;
-    prescaler = TC_CLOCK_PRESCALER_DIV256;
-    compare = (48000000/256)/freq;
-  } else if (freq >= 0.75) {
-    divider = 1024;
-    prescaler = TC_CLOCK_PRESCALER_DIV1024;
-    compare = (48000000/1024)/freq;
-  } else {
-    Serial.println("Invalid frequency");
-    while (1) delay(10);
-  }
-  */
 }
 
 void createSquareWave(float freq) {
